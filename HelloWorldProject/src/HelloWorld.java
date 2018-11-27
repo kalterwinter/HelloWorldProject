5ab21c49f3622bb92370ebd45982b0c2289293c5
kalterwinter <45207752+kalterwinter@users.noreply.github.com>
2018-11-27 17:49:32 +0100

    Update HelloWorld.java

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Klasse");
-		// das ist ein Kommentar, kein Code der Ausgeführt wird.
+		System.out.println("Hallo Rafael");
+		// das ist ein Kommentar, kein Code der AusgefÃ¼hrt wird.
 		// das ist ein einzeiliger Kommentar
 		/*
 		 * Mehrzeiligen Kommentar Schreiben
