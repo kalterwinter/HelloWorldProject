0ea4d125696c567ff8e99d9eb69ce20fc921a7df
rafael.winter <rafael.winter@KK-GYM-EDV1-03.gym.kk.local>
2018-11-27 17:45:20 +0100

    Kommentare hinzufügen

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Klasse");
-		
+		// das ist ein Kommentar, kein Code der Ausgeführt wird.
+		// das ist ein einzeiliger Kommentar
+		/*
+		 * Mehrzeiligen Kommentar Schreiben
+		 */
 	}
 
 }
