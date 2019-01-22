aea74bcab894f809166643f07b9e1655f4655d6b
rafael.winter <rafael.winter@KK-GYM-EDV1-03.gym.kk.local>
2019-01-22 18:05:03 +0100

    ArrayTest /Bubblesort

---
+++ HelloWorldProject/src/basics/WennDannSonst.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class WennDannSonst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//wenn - dann
+		if (1<3) {
+			System.out.println("1 < 3");
+		}
+		//
+		boolean trifft_zu = true;
+		if (trifft_zu == true) {
+			System.out.println("juhu");
+		}
+		//wenn dann sonst
+		int zahl = 3;
+		if (zahl==1) {
+			System.out.println("ungerade");
+		} else {
+			System.out.println("gerade");
+		}
+		//
+		
+	}
+}
---
+++ HelloWorldProject/src/sortieren/Bubblesort.java
@@ -0,0 +1,16 @@
+package sortieren;
+
+public class Bubblesort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int[] k= {3,1,9,5,2,8,6,5};
+	//
+	for (int n = 0; n< k.length; n++) {
+		for(int i=1; i<k.length;i++) {
+	  for (n=k.length; n>1; --n){ 
+		    for (i=0; i<n-1; ++i){ 
+		      if (k[i] > k[i+1]){ 
+		        k.swap(i, i+1)
+}}}}}}}
